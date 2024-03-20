c7aaf4a6807d5c4d518f4d41646c2f12ff76b5b0
ht3503 <152462027+ht3503@users.noreply.github.com>
2024-03-20 16:05:58 +0700

    Update bai1.c

--- bai1.c
+++ bai1.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
-#include <math.h>
-
-// Hàm kiểm tra xem một số có phải là số chính phương hay không
-int is_square_number(int num) {
-    int sqrt_num = sqrt(num);
-    return sqrt_num * sqrt_num == num;
-}
-
-// Hàm đếm và in ra các số chính phương nhỏ hơn n
-void count_and_print_square_numbers(int n) {
-    printf("Các số chính phương nhỏ hơn %d là:\n", n);
-    int count = 0;
-    for (int i = 1; i < n; i++) {
-        if (is_square_number(i)) {
-            printf("%d ", i);
-            count++;
-        }
-    }
-    printf("\nTổng cộng có %d số chính phương nhỏ hơn %d.\n", count, n);
-}
 
 int main() {
-    int n;
-    printf("Nhập số nguyên dương n: ");
-    scanf("%d", &n);
-    count_and_print_square_numbers(n);
+    printf("Cac so nguyen co 2 chu so va la boi cua 7:\n");
+    for (int i = 10; i <= 99; i++) {
+        if (i % 7 == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
     return 0;
 }
